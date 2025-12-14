883fec6adc2e21e51923d2df142024f73ba5832d
Paul Dumais <paul@unstate.ca>
2025-12-13 20:47:05 -0400

    new project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ca.cawala/seesaw_maps
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ca/cawala/seesaw_maps_test.clj
@@ -0,0 +1,7 @@
+(ns ca.cawala.seesaw-maps-test
+  (:require [clojure.test :refer :all]
+            [ca.cawala.seesaw-maps :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
