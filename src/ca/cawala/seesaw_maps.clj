9ef3ebac82f26c1428d5d56771d96212900d2764
Paul Dumais <paul@unstate.ca>
2025-12-27 16:20:04 -0400

    show regions bounded to map

--- src/ca/cawala/seesaw_maps.clj
+++ src/ca/cawala/seesaw_maps.clj
@@ -1,11 +1,12 @@
 (ns ca.cawala.seesaw-maps
   (:require
    [clojure.java.io :as io]
-   [honey.sql :as s]
+   [honey.sql :as sql]
    [next.jdbc :as jdbc]
    [next.jdbc.result-set :as rs]
    [org.corfield.logging4j2 :as logger]
    [seesaw.bind :as b]
+   [seesaw.tree :refer [simple-tree-model]]
    [seesaw.core :as s]
    [seesaw.mig :as m])
   (:import
@@ -16,18 +17,20 @@
    [net.wirelabs.jmaps.map.geo Coordinate])
   (:gen-class))
 
-(def db (d/init-db! "trie.db" {:pool-size 4}))
+(def db {:dbtype "sqlite" :dbname "trie.db"})
 (def ds (jdbc/get-datasource db))
 
-(defn get-region [path]
-  (jdbc/execute! ds (s/format {:select [:*] :from [:region]
-                               :where [:like :materialized-path path]})
-                 {:builder-fn rs/as-unqualified-kebab-maps}))
-
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(defn get-region
+  ([path]
+   (jdbc/execute! ds (sql/format {:select [:*] :from [:region]
+                                  :where [:like :materialized-path path]})
+                  {:builder-fn rs/as-unqualified-kebab-maps}))
+  ([lon lat depth]
+   (jdbc/execute! ds (sql/format {:select [:*] :from [:region]
+                                  :where [:and [:< :min-lon lon] [:< lon :max-lon]
+                                          [:< :min-lat lat] [:< lat :max-lat]
+                                          [:= [:length :materialized-path] depth]]})
+                  {:builder-fn rs/as-unqualified-kebab-maps})))
 
 (defn convert-to-px [map-viewer point]
   (let [zoom (.getZoom map-viewer)
@@ -66,8 +69,6 @@
     (when (not-empty boxes) (.setBestFit map-viewer boxes))
     (.repaint map-viewer)))
 
-(comment (-main))
-
 (defn zoom-tiles [map-viewer zoom]
   (let [base-layer (-> map-viewer .getCurrentMap .getBaseLayer)
         top-left-point (.getTopLeftCornerPoint map-viewer)
@@ -78,6 +79,14 @@
         mid-coordinate (.pixelToLatLon base-layer mid (.getZoom map-viewer))]
     (doto map-viewer (.setZoom zoom) (.centerOnLocation mid-coordinate) .repaint)))
 
+(defn nav-buttons []
+  (m/mig-panel :constraints ["" "[c]"]
+               :items [[(s/button :id :up :text "up") "wrap"]
+                       [(s/button :id :left :text "left") "split"]
+                       [(s/button :id :right :text "right") "wrap"]
+                       [(s/button :id :down :text "down") "wrap"]
+                       [(s/text :id :path :multi-line? false :columns 15 :halign :center) ""]]))
+
 (defn sliders [map-viewer]
   (let [i-zoom-slider (s/slider :id :i-zoom :orientation :vertical :major-tick-spacing 1
                                 :snap-to-ticks? true :paint-track? true :value 1
@@ -87,33 +96,41 @@
                                 :paint-labels? true :min 0 :max 19)]
     (b/bind i-zoom-slider (b/b-do [scale] (zoom-image map-viewer scale)))
     (b/bind t-zoom-slider (b/b-do [zoom] (zoom-tiles map-viewer zoom)))
-    (m/mig-panel :constraints ["wrap" "[c]" "[][]push[][]"]
-                 :items [["Image Zoom" ""]
-                         [i-zoom-slider ""]
-                         ["Tile Zoom" ""]
-                         [t-zoom-slider "height :400:"]])))
+    (m/mig-panel :constraints ["" "[c] [c]" "[] [top]"]
+                 :items [["Image Zoom" ""] ["Tile Zoom" "wrap"]
+                         [i-zoom-slider ""] [t-zoom-slider "wrap, height :400:"]
+                         [(nav-buttons) "span"]])))
 
-
-
-
-(defn show-boxes [coordinates]
-  (.setObjects box-painter coordinates))
+(defn show-region [{:keys [min-lon max-lon min-lat max-lat] :as region} map-viewer sliders]
+  (s/config! (s/select sliders [:#path]) :text (:materialized-path region))
+  (let [base-layer (-> map-viewer .getCurrentMap .getBaseLayer)
+        zoom (.getZoom map-viewer)
+        min-map-pt (.pixelToLatLon base-layer (Point. 0 0) zoom)
+        max-map-pt (.pixelToLatLon base-layer
+                                   (Point. (.width (.getMapSizeInPixels map-viewer zoom))
+                                           (.height (.getMapSizeInPixels map-viewer zoom)))
+                                   zoom)
+        bounded-min-lat (max min-lat (.getLatitude max-map-pt))
+        bounded-max-lat (min max-lat (.getLatitude min-map-pt))
+        _ (.setObjects box-painter [(Coordinate. min-lon bounded-min-lat)
+                                    (Coordinate. max-lon bounded-max-lat)])
+        boxes (.getObjects box-painter)]
+    (when (not-empty boxes) (.setBestFit map-viewer boxes))
+    (.repaint map-viewer)))
 
 (defn frame-content []
   (let [map-viewer (proxy [MapViewer] []
                      (setZoom [zoom]
                        (proxy-super setZoom zoom)
                        (s/config! (s/select (or (.getParent this) this) [:#t-zoom]) :value zoom)))
-        lublin-pl (Coordinate. 22.565628, 51.247717)
         sliders (sliders map-viewer)]
+    (def sliders sliders)
+    (def map-viewer map-viewer)
     (doto map-viewer
       (.setShowCoordinates true)
-      (.setZoom 12)
-      (.setHome lublin-pl)
       (.addUserOverlay box-painter)
       (.setCurrentMap (io/as-file (io/resource "OpenStreetMap.xml"))))
-    (show-boxes [(Coordinate. 22 51) (Coordinate. 23 52)])
-    (println (.getZoom map-viewer))
+    (show-region (first (get-region 22.565628 51.247717 2)) map-viewer sliders)
     (s/invoke-later (s/config! (s/select sliders [:#t-zoom]) :value (.getZoom map-viewer)))
     (m/mig-panel :constraints ["fill" "" ""]
                  :items [[map-viewer "grow"]
@@ -134,18 +151,13 @@
 
 (defn -main [& args]
   (System/setProperty "sun.java2d.opengl" "true")
-  (greet {:name (first args)})
   (FlatLightLaf/setup)
   (show!))
 
 (comment
   (-main)
   (use 'seesaw.dev)
-  (show-options (s/frame))
-  (s/select (.getParent map-viewer) [:#t-zoom])
-  (s/select sliders [:#t-zoom])
-  (s/full-screen! frame)
-  (s/full-screen! nil)
+  (show-region (first (get-region "b")) map-viewer sliders)
   (logger/as-marker :a :b)
   (logger/with-log-context {:id "tony"}
     (logger/info "Hello World Again"))
